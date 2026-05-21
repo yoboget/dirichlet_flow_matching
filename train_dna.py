144d8cb938108599b260817b5d6515f09400ab1d
yoann <yoann.boget@etu.unige.ch>
2026-05-21 17:43:51 +0200

    init

--- train_dna.py
+++ train_dna.py
@@ -11,7 +11,7 @@ from pytorch_lightning.callbacks import ModelCheckpoint
 
 if args.wandb:
     wandb.init(
-        entity="anonymized",
+        # entity="anonymized",
         settings=wandb.Settings(start_method="fork"),
         project="betawolf",
         name=args.run_name,
