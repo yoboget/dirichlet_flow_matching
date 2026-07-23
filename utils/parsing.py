85c7cdf2b0c0795f5f9898af3c3f3ecd925eef89
yoann <yoann.boget@etu.unige.ch>
2026-07-23 16:06:38 +0200

    init

--- utils/parsing.py
+++ utils/parsing.py
@@ -106,8 +106,8 @@ def parse_train_args():
     parser.add_argument('--num_integration_steps', type=int, default=100, help='The number of integration steps used during inference.')
 
     # Sampling
-    parser.add_argument("--sampling_score", type=float, default=0.0)
-    parser.add_argument("--flow_score", type=float, default=0.0)
+    parser.add_argument("--sampling_score", type=float, default=-1)
+    parser.add_argument("--flow_score", type=float, default=1.0)
 
     # Logging
     parser.add_argument("--no_tqdm", action="store_true")
