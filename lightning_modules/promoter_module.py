f02d2f97f98f8a419999e8ce852aee7b6c48f01f
yoann <yoann.boget@etu.unige.ch>
2026-08-29 14:15:16 +0200

    init

--- lightning_modules/promoter_module.py
+++ lightning_modules/promoter_module.py
@@ -206,7 +206,7 @@ class PromoterModule(GeneralModule):
                 # xt = (out_probs.unsqueeze(-1) * x_next).sum(-2)
 
                 S = (out_probs * scale).sum(-1, keepdim=True)  # (B, L, 1)
-                xt = xt * (S - out_probs * scale) + flow_probs * x_i_next
+                xt = xt * (S - out_probs * scale) + out_probs * x_i_next
             elif args.flow_method == 'unsid':
                 k = Categorical(out_probs).sample().to(self.device)
                 k_one_hot = F.one_hot(k, num_classes=out_probs.size(-1)).to(self.device)
